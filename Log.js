820c00c708f5ca6dca80bd3004e9a4ceaa0b0f17
MJxALPHA <78998499+MJxALPHA@users.noreply.github.com>
2021-05-22 12:49:41 +0530

    Add files via upload

---
+++ Log.js
@@ -0,0 +1,16 @@
+class Log extends BaseClass {
+    constructor(x, y, height, angle) {
+      var options = {
+          'restitution':0.8,
+          'friction':1.0,
+          'density':1.0
+      }
+      super(x,y,20,height,angle);
+      this.image = loadImage("sprites/wood2.png");
+      Matter.Body.setAngle(this.body,angle);
+    }
+    display(){
+      super.display();
+    }
+  };
+  
